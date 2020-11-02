28aaa0b68f22949eb50740158ab72c5570b6832f
Carl Bäckström <genzorg@gmail.com>
2020-11-02 09:23:46 +0100

    Celsium to Fahrenheit

---
+++ src/numbers.ts
@@ -0,0 +1,28 @@
+import { assert } from './utils';
+
+/**
+ * Converts a temperature in Celsius to Fahrenheit
+ *
+ * https://en.wikipedia.org/wiki/Fahrenheit
+ * https://www.jshero.net/en/koans/fahrenheit.html
+ *
+ * @param num Temperature in celsius
+ */
+function celsiusToFahrenheit(num: number) {
+  return num * (9 / 5) + 32;
+}
+
+/**
+ * Converts a temperature in Fahrenheit to Celsius
+ *
+ * https://en.wikipedia.org/wiki/Fahrenheit
+ * https://www.jshero.net/en/koans/fahrenheit.html
+ *
+ * @param num Temperature in Fahrenheit
+ */
+function fahrenheitToCelsius(num: number) {
+  return (num - 32) * (9 / 5);
+}
+
+console.log(assert(celsiusToFahrenheit(0), 32));
+console.log(assert(fahrenheitToCelsius(32), 0));
