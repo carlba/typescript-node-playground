dc8925c44209351a8dcb0bd9dd2711a3191f8015
Carl Bäckström <genzorg@gmail.com>
2020-11-09 07:56:07 +0100

    More math

--- src/math.ts
+++ src/math.ts
@@ -1,11 +1,94 @@
+import { match } from 'assert';
 import { assert } from './utils';
+/**
+ * Returns the last number in a decimal number
+ * @param num A number
+ */
+function onesDigit(num: number) {
+  return num % 10;
+}
 
 /**
+ * Returns true if number is even
+ * @param num A number
+ */
+function isEven(num: number) {
+  return num % 2 === 0;
+}
+
+/**
+ * Returns true if number is even
+ * @param num A number
+ */
+function isOdd(num: number) {
+  return num % 2 === 1;
+}
+/**
+ * Returns the mean of two numbers
+ *
+ * @param num1 Number
+ * @param num2 Number
+ */
+function mean(num1: number, num2: number) {
+  return (num1 + num2) / 2;
+}
+
+/**
+ * Returns the hypotenuse of two numbers
+ *
+ * Returns the midrange  of 3 numbers. The midrange is the mean of the smallest and largest number.
+ * https://www.jshero.net/en/koans/minmax.html
+ *
+ * @param num1 Number
+ * @param num2 Number
+ * @param num3 Number
+ */
+function midrange(...args: number[]) {
+  return mean(Math.max(...args), Math.min(...args));
+}
+
+/**
+ * Returns the hypotenuse of two numbers
+ *
+ * Write a function hypotenuse that calculates the length of the hypotenuse of a right triangle. The length of the two
+ * legs is passed to the function. Tip: In a right triangle the Pythagorean theorem is valid. If a and b are the
+ * lengths of the two legs and c is the length of the hypotenuse,
+ * the following is true: a² + b² = c². Since 3² + 4² = 5² applies, hypotenuse(3, 4) should return 5.
+ *
+ * @param num1 Number
+ * @param num2 Number
+ */
+function hypotenuse(num1: number, num2: number) {
+  return Math.sqrt(Math.pow(num1, 2) + Math.pow(num2, 2));
+}
+
+/**
+ * Returns the area of a circle.
+ *
+ * The area of a circle can be calculated, like so:
+ * `radius * radius * PI`
+ *
+ * @param radius The radius of a circle
+ */
+function areaOfCircle(radius: number) {
+  return radius * radius * Math.PI;
+}
+
+/**
+ * https://www.jshero.net/en/koans/round.html
  * @param num A float or integer
  * @returns The `num` parameter rounded to the closest hundred
  */
 function roundToClosestHundred(num: number) {
   return Math.round(num / 100) * 100;
+}
+
+function randomInt(min: number, max: number): number {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+function dice() {
+  return Math.floor(Math.random() * 6) + 1;
 }
 
 /**
@@ -79,26 +162,52 @@ function numInstr(str: string): number | null {
 }
 
 if (!module.parent) {
-  console.log(assert(roundToClosestHundred(49.99), 0));
-  console.log(assert(roundToClosestHundred(249), 200));
-  console.log(assert(roundToClosestHundred(101), 100));
+  console.log(assert(onesDigit(2764), 4));
+
+  // Modulo
+  console.log(54321 % 4);
+  console.log(54321 % 100);
+  console.log(54321 % 1000);
+  console.log(assert(isEven(4), true));
+  console.log(assert(isOdd(1), true));
+
+  // Mean and Median
+  console.log(assert(mean(10, 5), 7.5));
   console.log(assert(median([1, 5, 10, 20]), 7.5));
   console.log(assert(median([100000, 222, 1000, 10, 60]), 222));
+
+  // Geometry
+  console.log(assert(hypotenuse(3, 4), 5));
+  console.log(assert(midrange(3, 9, 1), 5));
+
+  // PI
+  console.log(assert(areaOfCircle(2), 4 * Math.PI));
+
+  // GCD
   console.log(assert(gcd(100, 96), 4));
+
+  // Prime
   const primeNumbers = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97];
   const nonPrimeNumbers = [0, 1, 4, 6, 8, 9];
-  primeNumbers.forEach(primeNumber => console.log(assert(isPrime(primeNumber), true)));
-  nonPrimeNumbers.forEach(nonPrimeNumber => console.log(assert(isPrime(nonPrimeNumber), false)));
+  primeNumbers.forEach(primeNumber =>
+    console.log(assert(isPrime(primeNumber), true, `${primeNumber} is prime number`))
+  );
+  nonPrimeNumbers.forEach(nonPrimeNumber =>
+    console.log(assert(isPrime(nonPrimeNumber), false, `${nonPrimeNumber} is not prime number`))
+  );
 
+  // Matrix
   const arrayOfNumbersArray: number[][] = [
     [1, 5, 10],
     [2, 5, 5],
     [5, 5, 3]
   ];
   console.log(sumMatrix(arrayOfNumbersArray));
+  console.log(assert(max(1, 10, 5), 10));
 
-  console.log(max(1, 10, 5));
-
-  console.log(numInstr('testar -15 20 test'));
-  console.log('test');
+  // Random
+  console.log(randomInt(2, 6));
+  console.log(randomInt(2, 6));
+  console.log(randomInt(2, 6));
+  console.log(randomInt(2, 6));
 }
